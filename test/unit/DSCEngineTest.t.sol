39ff84a0d49ccd75ca7e6c5c6fc945f355db0cff
CC <casinocompiler@gmail.com>
2024-09-11 18:32:15 +0100

    complete mint and burn testing

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -14,6 +14,8 @@ contract DSCEngineTest is Test {
     address bob = makeAddr("bob");
     address alice = makeAddr("alice");
 
+    uint256 constant MINT_AMOUNT = 100;
+    uint256 constant BURN_AMOUNT = 20;
     uint256 constant GAS_MONEY = 1 ether;
 
     function setUp() public {
@@ -50,28 +52,45 @@ contract DSCEngineTest is Test {
         dsc.mint(bob, 0);
     }
 
-    function test_CanMintDSC() public mintDSCToAccount(bob, 100){
+    function test_CanMintDSC() public mintDSCToAccount(bob, MINT_AMOUNT){
         assert(dsc.balanceOf(bob) == 100);
     }
 
-    function test_BurnErrorsOndBurningZero() public mintDSCToAccount(address(dscEngine), 100){
+    function test_mintEmitsEvent() public{
+        vm.prank(address(dscEngine));
+        vm.expectEmit(true,true,false,false,address(dsc));
+        emit TokensMinted(bob, MINT_AMOUNT);
+        dsc.mint(bob, MINT_AMOUNT);
+    }
+
+    function test_BurnErrorsOndBurningZero() public mintDSCToAccount(address(dscEngine), MINT_AMOUNT){
         vm.prank(address(dscEngine));
         vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__MustBurnMoreThanZero.selector);
         dsc.burn(0);
     }
 
-    function test_BurnErrorsOndBurningMoreThanBalance() public mintDSCToAccount(address(dscEngine), 100){
+    function test_BurnErrorsOndBurningMoreThanBalance() public mintDSCToAccount(address(dscEngine), MINT_AMOUNT){
         vm.prank(address(dscEngine));
         vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__BurnAmountGreaterThanUserBalance.selector);
         dsc.burn(101);
     }
 
-    function test_CanBurnDSC() public mintDSCToAccount(address(dscEngine), 100){
+    function test_CanBurnDSC() public mintDSCToAccount(address(dscEngine), MINT_AMOUNT){
         hoax(address(dscEngine), GAS_MONEY); 
         dsc.burn(20);
 
         assert(dsc.balanceOf(address(dscEngine)) == 80);
     }
+
+    function test_BurnEmitsEvent() public mintDSCToAccount(address(dscEngine), MINT_AMOUNT){
+        vm.prank(address(dscEngine));
+        vm.expectEmit(true,true,false,false,address(dsc));
+        emit TokensBurned(address(dscEngine), BURN_AMOUNT); 
+        dsc.burn(BURN_AMOUNT);
+
+        assert(dsc.balanceOf(address(dscEngine)) == 80);
+    }
+
     // Will implement later
     function test_CanRecoverERC() public{}
 
